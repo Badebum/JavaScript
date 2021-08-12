1701f709d38f3a6b0b7f0da1129bf9b0e69f9283
Andrey <82231196+Badebum@users.noreply.github.com>
2021-08-12 16:09:59 +0300

    дз №1

---
+++ js/hm2_01.js
@@ -0,0 +1,8 @@
+const name = 'генератор защитного поля';
+let price = 1000;
+
+console.log(`выбран ${name}, цена за штуку ${price} кредитов`);
+
+price = 2000;
+
+console.log(`выбран ${name}, цена за штуку ${price} кредитов`);
