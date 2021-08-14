43e38b05eb2fb191eba2bfd8aa3f040d74109ebd
Andrey <82231196+Badebum@users.noreply.github.com>
2021-08-14 18:23:00 +0300

    дз №2

---
+++ js/hm_02_03.js
@@ -0,0 +1,21 @@
+const findLongestWord = function(string) {
+  
+    const words = string.split(' ');
+    let bigestWord = words [0];
+
+    for (const word of words) {
+        if (word.length > bigestWord.length) {
+            bigestWord = word;
+        }
+    }
+return bigestWord;
+  };
+  
+  /*
+   * Вызовы функции для проверки работоспособности твоей реализации.
+   */
+  console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+  
+  console.log(findLongestWord('Google do a roll')); // 'Google'
+  
+  console.log(findLongestWord('May the force be with you')); // 'force'
---
+++ js/hm_02_04.js
@@ -0,0 +1,30 @@
+const formatString = function(string) {
+
+    let str = '';
+
+    if (string.length > 40) {
+        str = string.substr(0, 40) + '...';
+     } else {
+         str = string;
+     }
+
+     return str;
+  };
+  
+  /*
+   * Вызовы функции для проверки работоспособности твоей реализации.
+   */
+  console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+  // вернется оригинальная строка
+  
+  console.log(formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'));
+  // вернется форматированная строка
+  
+  console.log(formatString('Curabitur ligula sapien.'));
+  // вернется оригинальная строка
+  
+  console.log(
+    formatString(
+      'Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.',
+    ),
+  );
