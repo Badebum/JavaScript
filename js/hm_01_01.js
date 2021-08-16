409a4445233ff4c0e3736225c633cdd10de5a438
Andrey <82231196+Badebum@users.noreply.github.com>
2021-08-16 17:27:06 +0300

    4ewrt

--- js/hm_01_01.js
+++
@@ -1,8 +0,0 @@
-const name = 'генератор защитного поля';
-let price = 1000;
-
-console.log(`выбран ${name}, цена за штуку ${price} кредитов`);
-
-price = 2000;
-
-console.log(`выбран ${name}, цена за штуку ${price} кредитов`);
