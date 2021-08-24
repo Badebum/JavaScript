22f36a18ae56086f54b58d912f0b4eb538b7986d
Andrey <82231196+Badebum@users.noreply.github.com>
2021-08-24 19:30:00 +0300

    hm_04.js

--- js/hm_03_01.js
+++ js/hm_03_01.js
@@ -13,23 +13,3 @@ const keys = Object.keys(user);
 for (const key of keys) {
   console.log(`${key} : ${user[key]} `);
 }
-
-// const userUpdate = {
-//   name: 'Mango',
-//   age: 20,
-//   hobby: 'skydiving',
-//   premium: false,
-//   mood: 'happy',
-// };
-
-// const userNewProfile = {
-//   ...user,
-//   ...userUpdate,
-// };
-
-// console.log(userNewProfile);
-
-// const keys = Object.keys(userNewProfile);
-// for (const key of keys) {
-//   console.log(`${key} : ${userNewProfile[key]}`);
-// }
