ed0d96e340c5fc12583159fb26ac9ef077561fe2
Andrey <82231196+Badebum@users.noreply.github.com>
2021-08-27 15:52:58 +0300

    hm_05

---
+++ js/hm_05_04.js
@@ -0,0 +1,40 @@
+class StringBuilder {
+  constructor(str) {
+    this._value = str;
+  }
+
+  get value() {
+    return this._value;
+  }
+
+  append(str) {
+    this._value += str;
+  }
+
+  prepend(str) {
+    this._value = str + this._value;
+  }
+
+  pad(str) {
+    this.append(str);
+    this.prepend(str);
+  }
+
+  dablPad(str) {
+    this.pad(str);
+  }
+}
+
+const builder = new StringBuilder('.');
+
+builder.append('^');
+console.log(builder.value); // '.^'
+
+builder.prepend('^');
+console.log(builder.value); // '^.^'
+
+builder.pad('=');
+console.log(builder.value); // '=^.^='
+
+builder.dablPad('!!!');
+console.log(builder.value); // '=^.^='
