3afd052629cf225f6fa5479b04134c462d56b6eb
Andrey <82231196+Badebum@users.noreply.github.com>
2021-08-31 17:34:01 +0300

    Update hm_05_06.js

--- js/hm_05/hm_05_06.js
+++ js/hm_05/hm_05_06.js
@@ -1,9 +1,42 @@
-var myNoun = 'dog';
-var myAdjective = 'big';
-var myVerb = 'ran';
-var myAdverb = 'quickly';
+const count = 1;
 
-// Only change code below this line
-var wordBlanks = myNoun + ' ' + myVerb + ' ' + myAdjective + ' ' + myAdverb; // Change this line
-// Only change code above this line
-console.log(wordBlanks);
+function cc(card) {
+  // Only change code below this line
+  // получая карту я уменьшаю или увеличиваю count
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count += 1;
+      break;
+    case 7:
+    case 8:
+    case 9:
+      count += 0;
+      break;
+    case 10:
+    case 'J':
+    case 'Q':
+    case 'K':
+    case 'A':
+      count -= 1;
+      break;
+  }
+
+  // if если + значит Bet, есди 0 или -1 значит Hold
+  if (count > 0) {
+    return count + 'Bet';
+  } else if (count <= 0) {
+    return count + 'Hold';
+  }
+  return card;
+  // Only change code above this line
+}
+
+cc(2);
+cc(3);
+cc(7);
+cc('K');
+cc('A');
