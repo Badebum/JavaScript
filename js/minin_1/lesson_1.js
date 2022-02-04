0b829d6049a172326abd38300d41f5a60fcbd25f
Andrey <82231196+Badebum@users.noreply.github.com>
2022-02-04 17:00:03 +0300

    Update lesson_1.js

--- js/minin_1/lesson_1.js
+++ js/minin_1/lesson_1.js
@@ -1,59 +1,27 @@
-import users from '../hm_06/users.js';
-
-const getUserNames = users => users.map(a => a.name);
-
-// console.log(getUserNames(users));
-// [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
-
-const getUsersWithEyeColor = (users, color) =>
-  users.filter(({ eyeColor }) => eyeColor === color);
-
-// console.log(getUsersWithEyeColor(users, 'blue'));
-// [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
-
-const getUsersWithGender = (users, gen) =>
-  users.filter(({ gender }) => gender === gen);
-
-// console.log(getUsersWithGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
-
-const getInactiveUsers = users => users.filter(({ isActive }) => isActive);
-
-// console.log(getInactiveUsers(users)); // [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
-
-const getUserWithEmail = (users, mail) => users.find(a => a.email === mail);
-
-// console.log(getUserWithEmail(users, 'shereeanthony@kog.com')); // {объект пользователя Sheree Anthony}
-// console.log(getUserWithEmail(users, 'elmahead@omatom.com')); // {объект пользователя Elma Head}
-
-const getUsersWithAge = (users, min, max) =>
-  users.filter(a => a.age >= min && a.age <= max);
-
-// console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
-
-// console.log(getUsersWithAge(users, 30, 40));
-// [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
-
-const calculateTotalBalance = users =>
-  users.map(a => a.balance).reduce((a, b) => a + b, 0);
-
-// console.log(calculateTotalBalance(users)); // 20916
-
-const getUsersWithFriend = (users, friendName) =>
-  users.filter(({ friends }) => friends.includes(friendName)).map(a => a.name);
-
-// console.log(getUsersWithFriend(users, 'Briana Decker')); // [ 'Sharlene Bush', 'Sheree Anthony' ]
-// console.log(getUsersWithFriend(users, 'Goldie Gentry')); // [ 'Elma Head', 'Sheree Anthony' ]
-
-const getNamesSortedByFriendsCount = users =>
-  users.sort((a, b) => a.friends.length - b.friends.length).map(a => a.name);
-
-// console.log(getNamesSortedByFriendsCount(users));
-// [ 'Moore Hensley', 'Sharlene Bush', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony', 'Ross Vazquez' ]
-
-const getSortedUniqueSkills = users => {
-  let res = users.flatMap(({ skills }) => skills);
-  return res.filter((e, i) => res.indexOf(e) === i).sort();
+const countries = {
+  english: 'Welcome',
+  czech: 'Vitejte',
+  danish: 'Velkomst',
+  dutch: 'Welkom',
+  estonian: 'Tere tulemast',
+  finnish: 'Tervetuloa',
+  flemish: 'Welgekomen',
+  french: 'Bienvenue',
+  german: 'Willkommen',
+  irish: 'Failte',
+  italian: 'Benvenuto',
+  latvian: 'Gaidits',
+  lithuanian: 'Laukiamas',
+  polish: 'Witamy',
+  spanish: 'Bienvenido',
+  swedish: 'Valkommen',
+  welsh: 'Croeso',
 };
-//
-console.log(getSortedUniqueSkills(users));
-// [ 'adipisicing', 'amet', 'anim', 'commodo', 'culpa', 'elit', 'ex', 'ipsum', 'irure', 'laborum', 'lorem', 'mollit', 'non', 'nostrud', 'nulla', 'proident', 'tempor', 'velit', 'veniam' ]
+
+function greet(language) {
+  return countries[language] || 'Welcome';
+}
+
+console.log(greet('duhjktch'));
+console.log(greet('IP_ADDRESS_INVALID'));
+console.log(greet('englighjghjsh'));
