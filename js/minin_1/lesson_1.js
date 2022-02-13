ab30c095257baef1d07178502f37e35570252659
Andrey <82231196+Badebum@users.noreply.github.com>
2022-02-13 13:49:51 +0300

    promise

--- js/minin_1/lesson_1.js
+++ js/minin_1/lesson_1.js
@@ -1,33 +1,23 @@
-function sortByLength(array) {
-  return array.sort((a, b) => a.length - b.length);
-}
-console.log(
-  sortByLength([
-    'bitcoin',
-    'take',
-    'over',
-    'the',
-    'world',
-    'maybe',
-    'who',
-    'knows',
-    'perhaps',
-  ]),
-);
+const users = [
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: true },
+  { name: 'Lux', active: false },
+];
 
-console.log(
-  sortByLength([
-    'turns',
-    'out',
-    'random',
-    'test',
-    'cases',
-    'are',
-    'easier',
-    'than',
-    'writing',
-    'out',
-    'basic',
-    'ones',
-  ]),
-);
+const toggleUserState = (allUsers, userName) => {
+  return new Promise(resolve => {
+    const updatedUsers = allUsers.map(user =>
+      user.name === userName ? { ...user, active: !user.active } : user,
+    );
+    resolve(updatedUsers);
+  });
+};
+
+const logger = updatedUsers => console.table(updatedUsers);
+
+/*
+ * Должно работать так
+ */
+toggleUserState(users, 'Mango').then(logger);
+toggleUserState(users, 'Lux').then(logger);
