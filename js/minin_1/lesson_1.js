cdd0a573bd228f7edcb35bae539bbe6b5963cefd
Andrey <82231196+Badebum@users.noreply.github.com>
2022-02-13 12:13:47 +0300

    Update lesson_1.js

--- js/minin_1/lesson_1.js
+++ js/minin_1/lesson_1.js
@@ -1,17 +1,33 @@
-function diamond(n) {
-  if (n <= 0 || n % 2 === 0) return null;
-  let str = '';
-  for (let i = 0; i < n; i++) {
-    let len = Math.abs((n - 2 * i - 1) / 2);
-   
-    str += ' '.repeat(len);
-    str += '*'.repeat(n - 2 * len);
-    str += '\n';
-     console.log(len);
-  }
-  return str;
+function sortByLength(array) {
+  return array.sort((a, b) => a.length - b.length);
 }
+console.log(
+  sortByLength([
+    'bitcoin',
+    'take',
+    'over',
+    'the',
+    'world',
+    'maybe',
+    'who',
+    'knows',
+    'perhaps',
+  ]),
+);
 
-// console.log(diamond(1));
-console.log(diamond(3));
-// console.log(diamond(1));
+console.log(
+  sortByLength([
+    'turns',
+    'out',
+    'random',
+    'test',
+    'cases',
+    'are',
+    'easier',
+    'than',
+    'writing',
+    'out',
+    'basic',
+    'ones',
+  ]),
+);
