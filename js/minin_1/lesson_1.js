a839ed2d319c7f7f341f13a7c4a83266f08d74cc
Andrey <82231196+Badebum@users.noreply.github.com>
2022-02-16 14:54:08 +0300

    Update lesson_1.js

--- js/minin_1/lesson_1.js
+++ js/minin_1/lesson_1.js
@@ -1,5 +1,6 @@
 const arr = ['Hello world', 'This is a great solution', 'How are you'];
 
-const result = a => a.map(len => len.split(' ').length);
+const result = a =>
+  a.map(len => len.split(' ').length).reduce((a, b) => (a > b ? a : b));
 
 console.log(result(arr));
