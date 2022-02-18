ae09df2b940a46898419a12c9298ef1bea9ac568
Andrey <82231196+Badebum@users.noreply.github.com>
2022-02-18 17:36:42 +0300

    Update lesson_1.js

--- js/minin_1/lesson_1.js
+++ js/minin_1/lesson_1.js
@@ -1,11 +1,10 @@
-var twoSum = function (nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[j] == target - nums[i]) {
-        return [i, j];
-      }
-    }
-  }
-};
+function squareDigits(num) {
+  return num
+    .toString()
+    .split('')
+    .map(a => Number(a))
+    .map(a => a * a)
+    .join('');
+}
 
-console.log(twoSum([2, 7, 11, 15], 9));
+console.log(squareDigits(3212));
